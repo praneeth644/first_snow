75e5d90638b1b75e493f9d67da1e3cd12fa5bfe5
praneeth644 <131234843+praneeth644@users.noreply.github.com>
2023-07-25 13:59:21 +0530

    Update first_snow.py

--- first_snow.py
+++ first_snow.py
@@ -14,4 +14,4 @@ streamlit.dataframe(my_fruit_list)
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.multiselect("pick some fruits:", list(my_fruit_list.index)
-streamlit.dataframe(my_fruit_list)
+
