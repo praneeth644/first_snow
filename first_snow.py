3f69b12441bf3cb7559068fe82e8b1b19eaee539
praneeth644 <131234843+praneeth644@users.noreply.github.com>
2023-07-25 13:33:43 +0530

    Update first_snow.py

--- first_snow.py
+++ first_snow.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('my parents new healthy dinner')
-streamlit.header('breakfast menu')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
